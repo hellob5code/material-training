380bb79c06e078d4a420175848944ec90971d7a9
Eric Truong <etruong@excilys.com>
2014-12-31 04:53:44 +0100

    Create SearchableActivity

---
+++ app/src/main/java/com/training/android/materiel/ui/activity/SearchableActivity.java
@@ -0,0 +1,56 @@
+package com.training.android.materiel.ui.activity;
+
+import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
+import com.training.android.materiel.R;
+
+public class SearchableActivity extends Activity {
+
+    private static final String TAG = SearchableActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searchable);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_searchable, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        handleIntent(intent);
+    }
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            Log.d(TAG, "Search query=" + query);
+        }
+    }
+}
--- app/src/main/java/com/training/android/materiel/ui/fragment/TextFieldsFragment.java
+++ app/src/main/java/com/training/android/materiel/ui/fragment/TextFieldsFragment.java
@@ -1,16 +1,31 @@
 package com.training.android.materiel.ui.fragment;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 import com.training.android.materiel.R;
+import com.training.android.materiel.ui.activity.SearchableActivity;
 
 public class TextFieldsFragment extends Fragment {
 
+    @InjectView(R.id.text_fields_btn_search_filter) Button btnSearchSilter;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_text_fields, container, false);
+        View view = inflater.inflate(R.layout.fragment_text_fields, container, false);
+        ButterKnife.inject(this, view);
+        return view;
+    }
+
+    @OnClick(R.id.text_fields_btn_search_filter)
+    protected void onButtonClicked(View view) {
+        startActivity(new Intent(getActivity(), SearchableActivity.class));
     }
 }
