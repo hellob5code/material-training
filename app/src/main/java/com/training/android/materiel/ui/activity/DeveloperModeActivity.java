96f9e6f90bed63e680755deaaddf1e6a54b3ec78
Eric Truong <etruong@excilys.com>
2014-12-31 03:04:18 +0100

    Add home button action for DevMode an Settings activities

--- app/src/main/java/com/training/android/materiel/ui/activity/DeveloperModeActivity.java
+++ app/src/main/java/com/training/android/materiel/ui/activity/DeveloperModeActivity.java
@@ -1,7 +1,10 @@
 package com.training.android.materiel.ui.activity;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 import com.training.android.materiel.R;
 
 public class DeveloperModeActivity extends Activity {
@@ -10,5 +13,18 @@ public class DeveloperModeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_developer_mode);
+        ActionBar toolbar = getActionBar();
+        if (toolbar != null) {
+            toolbar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
