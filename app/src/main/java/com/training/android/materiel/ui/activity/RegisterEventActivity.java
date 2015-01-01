a5fd98e63a3a2454851709914a8325da1458fbdd
Eric Truong <etruong@excilys.com>
2015-01-01 23:24:24 +0100

    Add DatePicker and TimePicker DialogFragment, Implement in RegisterEventActivity

--- app/src/main/java/com/training/android/materiel/ui/activity/RegisterEventActivity.java
+++ app/src/main/java/com/training/android/materiel/ui/activity/RegisterEventActivity.java
@@ -1,8 +1,6 @@
 package com.training.android.materiel.ui.activity;
 
-import android.app.ActionBar;
-import android.app.Activity;
-import android.content.Intent;
+import android.app.*;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.StateListDrawable;
@@ -13,19 +11,29 @@ import android.util.StateSet;
 import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 import butterknife.*;
 import com.rengwuxian.materialedittext.MaterialEditText;
 import com.rengwuxian.materialedittext.validation.RegexpValidator;
 import com.training.android.materiel.R;
+import com.training.android.materiel.ui.dialog.DatePickerDialogFragment;
+import com.training.android.materiel.ui.dialog.TimePickerDialogFragment;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Calendar;
+import java.util.Set;
+import java.util.TimeZone;
 
-public class RegisterEventActivity extends Activity {
+public class RegisterEventActivity extends Activity implements DatePickerDialogFragment.OnDateSetListener, TimePickerDialogFragment.OnTimeSetListener {
 
     private static final String TAG = RegisterEventActivity.class.getSimpleName();
+
+    private static final String TAG_FROM_DATE_PICKER = "from_date_picker";
+    private static final String TAG_FROM_TIME_PICKER = "from_time_picker";
+    private static final String TAG_TO_DATE_PICKER = "to_date_picker";
+    private static final String TAG_TO_TIME_PICKER = "to_time_picker";
 
     @InjectView(R.id.register_event_medt_name) MaterialEditText medtName;
     @InjectView(R.id.register_event_medt_location) MaterialEditText medtLocation;
@@ -35,10 +43,20 @@ public class RegisterEventActivity extends Activity {
     @InjectView(R.id.register_event_medt_to_time) MaterialEditText medtToTime;
     @InjectView(R.id.register_event_medt_timezone) MaterialEditText medtTimezone;
 
+    private DateTimeFormatter mDateFormatter;
+    private DateTimeFormatter mTimeFormatter;
+
+    private DateTime mFromDateTime = new DateTime();
+    private DateTime mToDateTime = new DateTime().plusHours(1);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_event);
+
+        mDateFormatter = DateTimeFormat.forPattern("E, MMM d y");
+        mTimeFormatter = DateTimeFormat.forPattern("h:mm a");
+
         ButterKnife.inject(this);
         ActionBar toolbar = getActionBar();
         if (toolbar != null) {
@@ -46,8 +64,19 @@ public class RegisterEventActivity extends Activity {
             toolbar.setDisplayHomeAsUpEnabled(true);
         }
 
-        StateListDrawable arrowSelector = new StateListDrawable();
+        setupAccountViews();
+        setupDateTimeViews();
+        setupTimezoneViews();
+    }
+
+    private void setupAccountViews() {
+        // TODO:
+    }
+
+    private void setupDateTimeViews() {
         Drawable arrow;
+        StateListDrawable arrowSelector = new StateListDrawable();
+
         TypedValue tv = new TypedValue();
         if (getTheme().resolveAttribute(android.R.attr.colorPrimary, tv, true) && tv.resourceId > 0) {
             int selectedColor = getResources().getColor(tv.resourceId);
@@ -67,6 +96,23 @@ public class RegisterEventActivity extends Activity {
         medtToDate.setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, arrowSelector, null);
         medtToTime.setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, arrowSelector, null);
         medtTimezone.setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, arrowSelector, null);
+
+        String dateString;
+        String timeString;
+
+        dateString = mDateFormatter.print(mFromDateTime);
+        medtFromDate.setText(dateString);
+        timeString = mTimeFormatter.print(mFromDateTime);
+        medtFromTime.setText(timeString);
+
+        dateString = mDateFormatter.print(mToDateTime);
+        medtToDate.setText(dateString);
+        timeString = mTimeFormatter.print(mToDateTime);
+        medtToTime.setText(timeString);
+    }
+
+    private void setupTimezoneViews() {
+        // TODO:
     }
 
     @Override
@@ -89,28 +135,131 @@ public class RegisterEventActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable("from_datetime", mFromDateTime);
+        outState.putSerializable("to_datetime", mToDateTime);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mFromDateTime = (DateTime) savedInstanceState.getSerializable("from_datetime");
+        mToDateTime = (DateTime) savedInstanceState.getSerializable("to_datetime");
+    }
+
     @OnClick(R.id.register_event_medt_from_date)
     protected void onFromDateEditTextClick() {
-        Log.d(TAG, "register_event_medt_from_date");
+        if (getFragmentManager().findFragmentByTag(TAG_FROM_DATE_PICKER) == null) {
+            DialogFragment newFragment = newDatePickerDialogFragment(mFromDateTime);
+            newFragment.show(getFragmentManager(), TAG_FROM_DATE_PICKER);
+        }
     }
 
     @OnClick(R.id.register_event_medt_from_time)
     protected void onFromTimeEditTextClick() {
-        Log.d(TAG, "register_event_medt_from_time");
+        if (getFragmentManager().findFragmentByTag(TAG_FROM_TIME_PICKER) == null) {
+            DialogFragment newFragment = newTimePickerDialogFragment(mFromDateTime);
+            newFragment.show(getFragmentManager(), TAG_FROM_TIME_PICKER);
+        }
     }
 
     @OnClick(R.id.register_event_medt_to_date)
     protected void onToDateEditTextClick() {
-        Log.d(TAG, "register_event_medt_to_date");
+        if (getFragmentManager().findFragmentByTag(TAG_TO_DATE_PICKER) == null) {
+            DialogFragment newFragment = newDatePickerDialogFragment(mToDateTime);
+            newFragment.show(getFragmentManager(), TAG_TO_DATE_PICKER);
+        }
     }
 
     @OnClick(R.id.register_event_medt_to_time)
     protected void onToTimeEditTextClick() {
-        Log.d(TAG, "register_event_medt_to_time");
+        if (getFragmentManager().findFragmentByTag(TAG_TO_TIME_PICKER) == null) {
+            DialogFragment newFragment = newTimePickerDialogFragment(mToDateTime);
+            newFragment.show(getFragmentManager(), TAG_TO_TIME_PICKER);
+        }
+    }
+
+    private DialogFragment newDatePickerDialogFragment(DateTime dateTime) {
+        DialogFragment fragment = new DatePickerDialogFragment();
+
+        Bundle args = new Bundle();
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, dateTime.getYear());
+        c.set(Calendar.MONTH, dateTime.getMonthOfYear() - 1);
+        c.set(Calendar.DAY_OF_MONTH, dateTime.getDayOfMonth());
+        args.putSerializable(DatePickerDialogFragment.ARG_CALENDAR_DATE, c);
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+
+    private DialogFragment newTimePickerDialogFragment(DateTime dateTime) {
+        DialogFragment fragment = new TimePickerDialogFragment();
+
+        Bundle args = new Bundle();
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.HOUR_OF_DAY, dateTime.getHourOfDay());
+        c.set(Calendar.MINUTE, dateTime.getMinuteOfHour());
+        args.putSerializable(TimePickerDialogFragment.ARG_CALENDAR_TIME, c);
+        fragment.setArguments(args);
+
+        return fragment;
     }
 
     @OnClick(R.id.register_event_medt_timezone)
     protected void onTimezoneEditTextClick() {
         Log.d(TAG, "register_event_medt_timezone");
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialogFragment fragment, int year, int monthOfYear, int dayOfMonth) {
+        Object tag = fragment.getTag();
+        String dateString = mDateFormatter.print(new DateTime(year, monthOfYear + 1, dayOfMonth, 0, 0, 0));
+
+        if (tag.equals(TAG_FROM_DATE_PICKER)) {
+            mFromDateTime = copyDate(mFromDateTime, year, monthOfYear, dayOfMonth);
+            medtFromDate.setText(dateString);
+
+        } else if (tag.equals(TAG_TO_DATE_PICKER)) {
+            mToDateTime = copyDate(mToDateTime, year, monthOfYear, dayOfMonth);
+            medtToDate.setText(dateString);
+        }
+    }
+
+    @Override
+    public void onTimeSet(TimePickerDialogFragment fragment, int hourOfDay, int minute) {
+        Object tag = fragment.getTag();
+        String timeString = mTimeFormatter.print(new DateTime(0, 1, 1, hourOfDay, minute, 0));
+
+        if (tag.equals(TAG_FROM_TIME_PICKER)) {
+            mFromDateTime = copyTime(mFromDateTime, hourOfDay, minute);
+            medtFromTime.setText(timeString);
+
+        } else if (tag.equals(TAG_TO_TIME_PICKER)) {
+            mToDateTime = copyTime(mToDateTime, hourOfDay, minute);
+            medtToTime.setText(timeString);
+        }
+    }
+
+    /**
+     * The {@link java.util.Calendar} date used by {@link android.widget.DatePicker} has a month value between 0-11 for compatibility
+     * with {@link org.joda.time.DateTime} we offset by 1.
+     */
+    private DateTime copyDate(DateTime dateTime, int year, int monthOfYear, int dayOfMonth) {
+        return dateTime.withYear(year)
+                .withMonthOfYear(monthOfYear + 1)
+                .withDayOfMonth(dayOfMonth)
+                .withHourOfDay(dateTime.getHourOfDay())
+                .withMinuteOfHour(dateTime.getMinuteOfHour());
+    }
+
+    private DateTime copyTime(DateTime dateTime, int hourOfDay, int minute) {
+        return dateTime.withYear(dateTime.getYear())
+                .withMonthOfYear(dateTime.getMonthOfYear())
+                .withDayOfMonth(dateTime.getDayOfMonth())
+                .withHourOfDay(hourOfDay)
+                .withMinuteOfHour(minute);
     }
 }
---
+++ app/src/main/java/com/training/android/materiel/ui/dialog/DatePickerDialogFragment.java
@@ -0,0 +1,59 @@
+package com.training.android.materiel.ui.dialog;
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
+public class DatePickerDialogFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    public interface OnDateSetListener {
+        public void onDateSet(DatePickerDialogFragment fragment, int year, int monthOfYear, int dayOfMonth);
+    }
+
+    public static final String ARG_CALENDAR_DATE = "calendar_date";
+
+    private OnDateSetListener callback;
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            callback = (OnDateSetListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement DatePickerDialogFragment.OnDateSetListener");
+        }
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        int year;
+        int month;
+        int day;
+
+        Calendar arg = (Calendar) getArguments().getSerializable(ARG_CALENDAR_DATE);
+        if (arg != null) {
+            year = arg.get(Calendar.YEAR);
+            month = arg.get(Calendar.MONTH);
+            day = arg.get(Calendar.DAY_OF_MONTH);
+        } else {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            year = c.get(Calendar.YEAR);
+            month = c.get(Calendar.MONTH);
+            day = c.get(Calendar.DAY_OF_MONTH);
+        }
+
+        // Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        callback.onDateSet(this, year, monthOfYear, dayOfMonth);
+    }
+}
