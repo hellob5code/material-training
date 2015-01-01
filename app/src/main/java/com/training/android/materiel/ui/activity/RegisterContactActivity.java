aecb267334fb6747b73f67ccf981896c60c24a13
Eric Truong <etruong@excilys.com>
2015-01-02 00:38:05 +0100

    Add RegisterContact activity

---
+++ app/src/main/java/com/training/android/materiel/ui/activity/RegisterContactActivity.java
@@ -0,0 +1,41 @@
+package com.training.android.materiel.ui.activity;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.support.v4.app.NavUtils;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.training.android.materiel.R;
+
+public class RegisterContactActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_contact);
+        ActionBar toolbar = getActionBar();
+        if (toolbar != null) {
+            toolbar.setDisplayShowTitleEnabled(false);
+            toolbar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_validate, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+        } else if (id == R.id.action_validate) {
+            // TODO: Perform action.
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/training/android/materiel/ui/fragment/TextFieldsFragment.java
+++ app/src/main/java/com/training/android/materiel/ui/fragment/TextFieldsFragment.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import com.training.android.materiel.R;
+import com.training.android.materiel.ui.activity.RegisterContactActivity;
 import com.training.android.materiel.ui.activity.RegisterEventActivity;
 import com.training.android.materiel.ui.activity.SearchableActivity;
 
@@ -24,6 +25,11 @@ public class TextFieldsFragment extends Fragment {
     @OnClick(R.id.text_fields_btn_register_event)
     protected void onRegisterEventButtonClicked() {
         startActivity(new Intent(getActivity(), RegisterEventActivity.class));
+    }
+
+    @OnClick(R.id.text_fields_btn_register_contact)
+    protected void onRegisterContactButtonClicked() {
+        startActivity(new Intent(getActivity(), RegisterContactActivity.class));
     }
 
     @OnClick(R.id.text_fields_btn_search_filter)
