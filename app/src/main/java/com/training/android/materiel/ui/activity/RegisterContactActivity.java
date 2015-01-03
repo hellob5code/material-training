bc159ce56e8e58e218e642a521d90e2d0c274ba7
Eric Truong <etruong@excilys.com>
2015-01-03 11:03:18 +0100

    Add contact spinners

--- app/src/main/java/com/training/android/materiel/ui/activity/RegisterContactActivity.java
+++ app/src/main/java/com/training/android/materiel/ui/activity/RegisterContactActivity.java
@@ -6,18 +6,42 @@ import android.support.v4.app.NavUtils;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 import com.training.android.materiel.R;
+import com.training.android.materiel.model.Contact;
+import com.training.android.materiel.util.EnumUtils;
 
 public class RegisterContactActivity extends Activity {
+
+    @InjectView(R.id.register_contact_spi_phone_type) Spinner spiPhoneType;
+    @InjectView(R.id.register_contact_spi_email_type) Spinner spiEmailType;
+    @InjectView(R.id.register_contact_spi_address_type) Spinner spiAddressType;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_contact);
+        ButterKnife.inject(this);
         ActionBar toolbar = getActionBar();
         if (toolbar != null) {
             toolbar.setDisplayShowTitleEnabled(false);
         }
+
+        ArrayAdapter<String> adapter;
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, EnumUtils.capitalizedFullyNames(Contact.PhoneType.class));
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spiPhoneType.setAdapter(adapter);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, EnumUtils.capitalizedFullyNames(Contact.EmailType.class));
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spiEmailType.setAdapter(adapter);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, EnumUtils.capitalizedFullyNames(Contact.AddressType.class));
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spiAddressType.setAdapter(adapter);
     }
 
     @Override
