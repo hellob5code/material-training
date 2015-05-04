31947f9c48dd1e83e9cacfef1689eaf63c043866
Eric Truong <etruong@excilys.com>
2015-05-04 21:53:21 +0200

    Add EnvironmentFragment text

--- app/src/main/java/fr/erictruong/training/material/ui/fragment/EnvironmentFragment.java
+++ app/src/main/java/fr/erictruong/training/material/ui/fragment/EnvironmentFragment.java
@@ -1,15 +1,14 @@
 package fr.erictruong.training.material.ui.fragment;
 
 import fr.erictruong.training.material.R;
-import fr.erictruong.training.material.ui.card.Card;
-import fr.erictruong.training.material.ui.card.DisplayBodyCard;
-import fr.erictruong.training.material.ui.card.HeadlineBodyCard;
+import fr.erictruong.training.material.ui.card.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static android.support.v7.widget.RecyclerView.NO_ID;
 import static fr.erictruong.training.material.ui.card.Card.TYPE_PRIMARY_DISPLAY_1_BODY_2;
+import static fr.erictruong.training.material.ui.card.Card.TYPE_PRIMARY_HEADLINE_BODY_1;
 import static fr.erictruong.training.material.ui.card.Card.TYPE_PRIMARY_HEADLINE_BODY_2;
 
 public class EnvironmentFragment extends MaterialTrainingNavigationDrawerFragment {
@@ -18,6 +17,8 @@ public class EnvironmentFragment extends MaterialTrainingNavigationDrawerFragmen
     protected List<Card> populateDataset() {
         List<Card> dataset = new ArrayList<>();
         dataset.add(new DisplayBodyCard(NO_ID, TYPE_PRIMARY_DISPLAY_1_BODY_2, getString(R.string.fragment_environment), null));
+        dataset.add(new HeadlineBodyCard(NO_ID, TYPE_PRIMARY_HEADLINE_BODY_1, getString(R.string.fragment_environment_3d_world), getString(R.string.fragment_environment_3d_world_txt)));
+        dataset.add(new HeadlineBodyCard(NO_ID, TYPE_PRIMARY_HEADLINE_BODY_1, getString(R.string.fragment_environment_light_and_shadow), getString(R.string.fragment_environment_light_and_shadow_txt)));
         return dataset;
     }
 }
