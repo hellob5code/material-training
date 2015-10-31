53bcecee3c71620d1b65bc9e19e24b166cf1a946
Eric Truong <eric@erictruong.fr>
2015-10-31 21:42:22 +0100

    DisplayBodyViewHolder

--- app/src/main/java/fr/erictruong/material/training/ui/components/cards/DisplayBodyViewHolder.java
+++ app/src/main/java/fr/erictruong/material/training/ui/components/cards/DisplayBodyViewHolder.java
@@ -1,25 +1,49 @@
 package fr.erictruong.material.training.ui.components.cards;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Space;
 import android.widget.TextView;
 
 import fr.erictruong.material.training.R;
 
 public class DisplayBodyViewHolder extends RecyclerView.ViewHolder implements MaterialCardViewHolder<DisplayBodyItem> {
 
+    @NonNull
     private TextView display;
+    @NonNull
     private TextView body;
+    @NonNull
+    private Space spaceDisplayBody;
 
     public DisplayBodyViewHolder(View itemView) {
         super(itemView);
         display = (TextView) itemView.findViewById(R.id.display);
         body = (TextView) itemView.findViewById(R.id.body);
+        spaceDisplayBody = (Space) itemView.findViewById(R.id.space_display_body);
     }
 
     @Override
     public void bind(DisplayBodyItem item) {
-        display.setText(item.display);
-        body.setText(item.body);
+        if (item.display == null || item.display.length() == 0) {
+            display.setVisibility(View.GONE);
+        } else {
+            display.setVisibility(View.VISIBLE);
+            display.setText(item.display);
+        }
+
+        if (item.body == null || item.body.length() == 0) {
+            body.setVisibility(View.GONE);
+        } else {
+            body.setVisibility(View.VISIBLE);
+            body.setText(item.body);
+        }
+
+        if (display.getVisibility() == View.VISIBLE && body.getVisibility() == View.VISIBLE) {
+            spaceDisplayBody.setVisibility(View.VISIBLE);
+        } else {
+            spaceDisplayBody.setVisibility(View.GONE);
+        }
     }
 }
